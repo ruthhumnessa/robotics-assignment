682353a1bf5b2638157652d68d45f8202bfeeb8c
ruthhumnessa <ruthjchumnessa@gmail.com>
2021-05-26 22:20:10 +0300

    Question two

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/ruth/arm_ws/src
+# Build directory: /home/ruth/arm_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("arm_description")
+subdirs("arm_gazebo")
---
+++ build/arm_gazebo/CMakeFiles/arm_model_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ruth/arm_ws/devel/lib/libarm_model_plugin.pdb"
+  "/home/ruth/arm_ws/devel/lib/libarm_model_plugin.so"
+  "CMakeFiles/arm_model_plugin.dir/src/controller.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arm_model_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
